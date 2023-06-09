a4e4f0a5881251c11cce21e844761d2f39846d91
curoles <igor@pop-os.localdomain>
2023-06-09 01:49:07 -0600

    better fix for https://github.com/sheredom/utf8.h/issues/109

--- src/include/smartarr/third/utf8.h
+++ src/include/smartarr/third/utf8.h
@@ -676,14 +676,12 @@ utf8_int8_t *utf8ncpy(utf8_int8_t *utf8_restrict dst,
   }
 
   for (check_index = index - 1;
-       //check_index > 0 && 0x80 == (0xc0 & d[check_index]);
-       check_index > 0 && (0x80 & d[check_index]);
-       check_index--) {
+       check_index > 0 && 0x80 == (0xc0 & d[check_index]); check_index--) {
     /* just moving the index */
   }
 
   if (check_index < index &&
-      (index - check_index) < utf8codepointsize(d[check_index])) {
+      (index - check_index) < utf8codepointcalcsize(&d[check_index])) {
     index = check_index;
   }
 
